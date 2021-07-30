fd87d5dc776b5d316c3c77067aaac7b104caae4c
Chris Wong <wc38.wong@gmail.com>
2021-07-31 07:58:41 +0800

    Theme

--- src/plugins/vuetify.ts
+++ src/plugins/vuetify.ts
@@ -4,4 +4,15 @@ import "vuetify/dist/vuetify.min.css";
 
 Vue.use(Vuetify);
 
-export default new Vuetify({});
+export default new Vuetify({
+  theme: {
+    themes: {
+      light: {
+        primary: "#17242d",
+      },
+      dark: {
+        primary: "#17242d",
+      },
+    },
+  },
+});
